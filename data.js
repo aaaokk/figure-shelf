9700f50bd73b92d9b25991589bbddeebf29c2b7f
aaaokk <teppei253@gmail.com>
2026-09-26 23:14:58 +0900

    Update data.js

--- data.js
+++ data.js
@@ -7,9 +7,7 @@ const figures = [
     series: "作品名",
     character: "キャラクター名",
     scale: "1/7",
-
-    image: "figure-a.jpg.jpeg",
-
+    image: "images/figure-a.jpg",
     description: "このフィギュアについての説明です。"
   },
 
@@ -20,9 +18,7 @@ const figures = [
     series: "作品名",
     character: "キャラクター名",
     scale: "1/6",
-
     image: "",
-
     description: "このフィギュアについての説明です。"
   }
 
@@ -33,37 +29,23 @@ const posts = [
 
   {
     id: "post-001",
-
     figureId: "figure-a",
-
     owner: "OWNER 01",
-
     image: "",
-
     date: "2026.09.01",
-
     location: "TOKYO",
-
     note: "自宅の棚に飾っています。",
-
     comment: ""
   },
 
   {
     id: "post-002",
-
     figureId: "figure-a",
-
     owner: "OWNER 02",
-
     image: "",
-
     date: "2026.08.15",
-
     location: "OSAKA",
-
     note: "旅行先に持っていきました。",
-
     comment: ""
   }
 
